8f434bb4553082087cf512882a3a3c0cd5dee26b
larsbf <larsbf@gmail.com>
2017-02-26 18:23:23 +0100

    Added commands

---
+++ src/commands/attack.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const _ = require('lodash')
+const config = require('../config')
+
+const msgDefaults = {
+    response_type: 'in_channel',
+    username: config('USERNAME'),
+    text: "Not implemented :hankey:",
+    icon_emoji: config('ICON_EMOJI')
+}
+
+const handler = (payload, res) => {
+    let msg = _.defaults({
+        channel: payload.channel_name,
+    }, msgDefaults)
+
+    res.set('content-type', 'application/json')
+    res.status(200).json(msg)
+
+    return
+}
+
+
+
+module.exports = {
+    pattern: /attack/ig,
+    handler: handler
+}
--- src/commands/repos.js
+++ src/commands/repos.js
@@ -1,4 +1,3 @@
-
 'use strict'
 
 const _ = require('lodash')
@@ -6,33 +5,36 @@ const config = require('../config')
 const trending = require('github-trending')
 
 const msgDefaults = {
-  response_type: 'in_channel',
-  username: config('USERNAME'),
-  icon_emoji: config('ICON_EMOJI')
+    response_type: 'in_channel',
+    username: config('USERNAME'),
+    icon_emoji: config('ICON_EMOJI')
 }
 
 const handler = (payload, res) => {
-  trending('javascript', (err, repos) => {
-    if (err) throw err
+    trending('javascript', (err, repos) => {
+        if (err) throw err
 
-    var attachments = repos.slice(0, 5).map((repo) => {
-      return {
-        title: `${repo.owner}/${repo.title} `,
-        title_link: repo.url,
-        text: `_${repo.description}_\n${repo.language} • ${repo.star}>`,
-        mrkdwn_in: ['text', 'pretext']
-      }
+        var attachments = repos.slice(0, 5).map((repo) => {
+            return {
+                title: `${repo.owner}/${repo.title} `,
+                title_link: repo.url,
+                text: `_${repo.description}_\n${repo.language} • ${repo.star}>`,
+                mrkdwn_in: ['text', 'pretext']
+            }
+        })
+
+        let msg = _.defaults({
+            channel: payload.channel_name,
+            attachments: attachments
+        }, msgDefaults)
+
+        res.set('content-type', 'application/json')
+        res.status(200).json(msg)
+        return
     })
-
-    let msg = _.defaults({
-      channel: payload.channel_name,
-      attachments: attachments
-    }, msgDefaults)
-
-    res.set('content-type', 'application/json')
-    res.status(200).json(msg)
-    return
-  })
 }
 
-module.exports = { pattern: /repos/ig, handler: handler }
+module.exports = {
+    pattern: /repos/ig,
+    handler: handler
+}
